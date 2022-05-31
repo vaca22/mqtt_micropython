da4a403499d217e61f025dc927c7ad7f09f8069b
vaca22 <wsmslgh@gmail.com>
2022-05-31 22:18:15 +0800

    fuck

---
+++ boot.py
@@ -0,0 +1,33 @@
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+ssid = 'vaca'
+password = '22345678'
+mqtt_server = '192.168.6.106'
+
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'notification'
+topic_pub = b'hello'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
